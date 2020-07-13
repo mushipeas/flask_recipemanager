5c1170713ab0d671a7537cd2490bf2c5ac1677a3
mushipeas <8151412+mushipeas@users.noreply.github.com>
2020-07-13 03:46:58 +0100

    reroute /newsearch to /search

--- app.py
+++ app.py
@@ -29,46 +29,6 @@ def index():
 
 
 @app.route("/search/", methods=["GET", "POST"])
-def search():
-    search_params = request.args.to_dict()
-    category = search_params["category"] if "category" in search_params.keys() else None
-    terms_ = search_params["terms"] if "terms" in search_params.keys() else None
-
-    if "p" not in search_params.keys():
-        search_params["p"] = "1"
-    page = int(search_params["p"])
-
-    if terms_:
-        terms = ["%" + term + "%" for term in terms_.split()]
-    else:
-        terms = [""]
-
-    if category in categories:
-        results_ = db.session.query(Recipe).filter(Recipe.category == category)
-    else:
-        results_ = db.session.query(Recipe)
-
-    for term in terms:
-        results_ = results_.filter(Recipe.title.like(term))
-
-    results_per_page = 12
-    result_offset = (page - 1) * results_per_page
-
-    results_count = results_.count()
-    results = results_.offset(result_offset).limit(results_per_page).all()
-
-    print(results_count)
-
-    return render_template(
-        "search_res.html",
-        results=results,
-        results_count=results_count,
-        **search_params,
-        next_page=page + 1
-    )
-
-
-@app.route("/newsearch/", methods=["GET", "POST"])
 def newsearch():
     search_params = request.args.to_dict()
     category = search_params["category"] if "category" in search_params.keys() else None
